6816fd1fbf26647bd3390e768ee5176018f7fe96
datmemerboi <shravanssd@gmail.com>
2022-01-28 18:58:07 +0530

    Update api utils

--- utils/api.js
+++ utils/api.js
@@ -19,6 +19,66 @@ async function fetchAllPatients() {
   }
 }
 
+async function fetchAllInvoices() {
+  let url, res, data = {}, error = {};
+  try {
+    url = process.env?.REACT_APP_ENVIRONMENT === "local"
+      ? '/api/invoice/all'
+      : config.API_URL + '/api/invoice/all';
+    res = await fetch(url);
+    if (res.status === 200) {
+      data = await res.json();
+    } else {
+      error = res;
+    }
+  } catch (e) {
+    error = e;
+  } finally {
+    return { data, error };
+  }
+}
+
+async function printInvoice(invid) {
+  let url, res, data = {}, error = {};
+  try {
+    url = process.env?.REACT_APP_ENVIRONMENT === "local"
+      ? `/api/invoice/print/${invid}`
+      : config.API_URL + `/api/invoice/print/${invid}`;
+    res = await fetch(url);
+    if (res.status === 200) {
+      data = await res.json();
+    } else {
+      error = res;
+    }
+  } catch (e) {
+    error = e;
+  } finally {
+    return { data, error };
+  }
+}
+
+async function fetchAppointmentsByDate(start, end) {
+  let url, res, data = {}, error = {};
+  try {
+    url = process.env?.REACT_APP_ENVIRONMENT === "local"
+    ? `/api/appointment/all/?from=${start}&to=${end}`
+    : config.API_URL + `/api/appointment/all/?from=${start}&to=${end}`
+    res = await fetch(url);
+    if (res.status === 200) {
+      data = await res.json();
+    } else {
+      error = res;
+    }
+  } catch (e) {
+    error = e;
+  } finally {
+    return { data, error };
+  }
+}
+
 export default {
-  fetchAllPatients
+  fetchAllPatients,
+  fetchAllInvoices,
+  printInvoice,
+  fetchAppointmentsByDate
 };
