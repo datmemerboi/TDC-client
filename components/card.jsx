dc3d65d50e85934849f27d91686864e5bce78ee0
datmemerboi <shravanssd@gmail.com>
2022-01-29 19:52:05 +0530

    Update cards component

--- components/card.jsx
+++ components/card.jsx
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
 
-export default function InvoiceCard({ obj, key, returnToParent }) {
+export const InvoiceCard = function ({ obj, key, returnToParent }) {
   console.log(obj);
   return (
     <div className="output-card" key={key}>
@@ -18,8 +18,7 @@ export default function InvoiceCard({ obj, key, returnToParent }) {
       ) : null}
       {
         <p>
-          <strong>Generated on: </strong>
-          {dayjs(obj.created_at).format('D MMM YYYY')}
+          <strong>Generated on:</strong> {dayjs(obj.created_at).format('D MMM YYYY')}
         </p>
       }
       <a className="beauty" onClick={() => returnToParent(obj.inv_id)}>
@@ -27,4 +26,36 @@ export default function InvoiceCard({ obj, key, returnToParent }) {
       </a>
     </div>
   );
-}
+};
+
+export const TreatmentCard = function ({ obj, key, onClickHandler }) {
+  return (
+    <div className="output-card" key={key}>
+      <h2>{obj.t_id}</h2>
+      <h3>{obj.p_id}</h3>
+      {obj.procedure_done ? (
+        <p>
+          <strong>Procedure:</strong> {obj.procedure_done}
+        </p>
+      ) : null}
+      {obj.doctor ? (
+        <p>
+          <strong>Doctor:</strong> {obj.doctor}
+        </p>
+      ) : null}
+      {obj.remarks ? (
+        <p>
+          <strong>Remarks:</strong> {obj.remarks}
+        </p>
+      ) : null}
+      {obj.treatment_date ? (
+        <p>
+          <strong>Procedure date:</strong> {dayjs(obj.treatment_date).format('D MMM YYYY')}
+        </p>
+      ) : null}
+      <a className="beauty" onClick={() => onClickHandler(obj.t_id)}>
+        <em>Create Invoice</em>
+      </a>
+    </div>
+  );
+};
