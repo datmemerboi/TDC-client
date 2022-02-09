b8ff38bac224a03c1624ead0cdf6b6404af04c5b
datmemerboi <shravanssd@gmail.com>
2022-02-09 23:57:25 +0530

    fix: Treatment -> Payment flow

--- components/payment.jsx
+++ components/payment.jsx
@@ -9,7 +9,6 @@ export function PaySlip({ list }) {
   let url = `/treatment/payment/${list.length ? list.join('-') : ''}`;
 
   useEffect(async () => {
-    console.log(list);
     let { data } = await api.checkCompatibility(list); // Check compatibility
     if (data && Object.prototype.hasOwnProperty.call(data, 'compatible')) {
       setCompatible(data.compatible);
@@ -56,51 +55,46 @@ export function PaySlip({ list }) {
   return null;
 }
 
-export function PayRow({ tid, returnToParent }) {
-  let [treatmentData, setTreatmentData] = useState({});
-  let [price, setPrice] = useState({ cost: 1, qty: 1, total: 1 });
+export function PayRow({ data, index, returnToParent }) {
+  const [cost, setCost] = useState(1);
+  const [qty, setQty] = useState(1);
 
   useEffect(async () => {
-    // Fetch data upon mount
-    if (tid && Object.keys(treatmentData).length === 0) {
-      let { data } = await api.getTreatmentData(tid);
-      if (Object.keys(data).length) {
-        setTreatmentData(data);
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    if (price.cost > -1 && price.qty > -1) {
-      setPrice({
-        ...price,
-        total: price.cost * price.qty
+    if (cost > -1 && qty > -1) {
+      returnToParent(index, {
+        ...data,
+        cost: cost,
+        qty: qty,
+        total: cost * qty
       });
-      returnToParent(tid, price.cost * price.qty);
     }
-  }, [price.cost, price.qty]);
+  }, [cost, qty]);
 
   const handleInput = (e) => {
     let { name, value } = e.target;
     if (!isNaN(value) && isFinite(value)) {
-      value = name === 'qty' ? parseInt(value) : parseFloat(value);
-      setPrice({ ...price, [name]: value });
+      if (name === 'qty') {
+        setQty(parseInt(value));
+      }
+      if (name === 'cost') {
+        setCost(parseFloat(value));
+      }
     }
   };
 
-  if (!treatmentData.procedure_done || !treatmentData.treatment_date) {
+  if (!data.procedure_done || !data.treatment_date) {
     return <tr />;
   } else {
     return (
       <Fragment>
         <tr>
           <td className="wide-col-1">
-            <span>{treatmentData.procedure_done}</span>
+            <span>{data.procedure_done.trim()}</span>
             <br />
-            <span>{dayjs(treatmentData.treatment_date).format('D MMM YYYY')}</span>
+            <span>{dayjs(data.treatment_date).format('D MMM YYYY')}</span>
             <br />
-            {treatmentData?.teeth_num && treatmentData.teeth_num?.length ? (
-              <span>{treatmentData.teeth_num.join(',')}</span>
+            {data?.teeth_num && data.teeth_num?.length ? (
+              <span>{data.teeth_num.join(',')}</span>
             ) : null}
           </td>
           <td>
@@ -109,7 +103,7 @@ export function PayRow({ tid, returnToParent }) {
               className="input-bar small-input-bar"
               min={1}
               name="cost"
-              value={price.cost}
+              value={cost}
               onInput={handleInput}
             />
           </td>
@@ -120,11 +114,11 @@ export function PayRow({ tid, returnToParent }) {
               min={0}
               style={{ width: '2vw' }}
               name="qty"
-              value={price.qty}
+              value={qty}
               onInput={handleInput}
             />
           </td>
-          <td>{price.total}</td>
+          <td>{isFinite(qty) && isFinite(cost) ? qty * cost : 0}</td>
         </tr>
       </Fragment>
     );
--- pages/treatment/payment/[list].jsx
+++ pages/treatment/payment/[list].jsx
@@ -1,59 +1,153 @@
 import { Fragment, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
+import NavBar from '../../../components/navbar';
+import Modal from '../../../components/modal';
 import { PayRow } from '../../../components/payment';
+import config from '../../../config.json';
+import api from '../../../utils/api';
 
 export default function TreatmentPayment(props) {
   const router = useRouter();
 
-  const [treatmentIDs, setTreatmentIDs] = useState([]);
-  const [priceList, setPriceList] = useState([]);
-  const [total, setTotal] = useState({ sub_total: 1, discount: 10, grand_total: 1 });
+  const [treatments, setTreatments] = useState([]);
+  const [total, setTotal] = useState({ sub_total: 0, discount: 0, grand_total: 0 });
+  const [payment, setPayment] = useState({});
+  const [modal, setModal] = useState({ show: false, message: '' });
 
-  useEffect(() => {
+  useEffect(async () => {
     let { list } = router.query;
     if (list) {
-      setTreatmentIDs(list.split('-')); // treatmentIDs: [TID1, TID2, TID3]
-      setPriceList(new Array(list.split('-').length).fill(1)); // priceList: [1, 1, 1]
+      let res = await Promise.all(list.split('-').map(api.getTreatmentData));
+      let data = res.map(({ data }) => ({ ...data, qty: 1, cost: 1, total: 1 }));
+      setTreatments(data);
     }
   }, []);
 
   useEffect(() => {
-    let subTotal = priceList.reduce((acc, val) => acc + val, 0);
+    let subTotal = treatments.reduce((acc, t) => {
+      if (!isNaN(t.total)) {
+        return acc + t.total;
+      }
+      return acc;
+    }, 0);
     let grandTotal = parseFloat(subTotal * (1 - total.discount / 100)).toFixed(2);
 
     setTotal({ ...total, sub_total: subTotal, grand_total: grandTotal });
-  }, [priceList]);
+  }, [treatments, total.discount]);
 
-  const updatePriceOfTreatment = (tid, value) => {
-    let updatedPriceList = [...priceList]; // Copy original array
-    updatedPriceList.splice(treatmentIDs.indexOf(tid), 1, value); // Replace index as of tID array
-    setPriceList(updatedPriceList);
+  const updatePriceOfTreatment = (index, obj) => {
+    let updated = [...treatments]; // Copy original array
+    updated.splice(index, 1, obj);
+    setTreatments(updated);
+  };
+
+  const handleInput = (e) => {
+    let { name, value } = e.target;
+    if (name === 'payment_method' || name === 'payment_id') {
+      setPayment({ ...payment, [name]: value });
+      return;
+    }
+    if (name === 'discount') {
+      if (!isNaN(value)) {
+        setTotal({ ...total, discount: parseFloat(value) });
+        return;
+      }
+    }
+  };
+
+  const handleSubmit = async () => {
+    let invoiceObj = {
+      p_id: treatments[0].p_id,
+      treatments: JSON.parse(JSON.stringify(treatments)),
+      payment_method: payment.payment_method,
+      payment_id: payment.payment_id ?? null,
+      sub_total: total.sub_total,
+      discount: total.discount,
+      grand_total: total.grand_total
+    };
+
+    let { data, error } = await api.createInvoice(invoiceObj);
+    if (error?.message) {
+      setModal({ show: true, message: error.message });
+    } else {
+      setModal({ show: true, message: `Invoice created, ID: ${data.inv_id}` });
+    }
   };
 
   return (
     <Fragment>
+      <NavBar />
       <div className="container">
         <h3>NEW INVOICE</h3>
         <table>
           <thead>
             <tr>
-              <th>Procedures</th>
+              <th>Procedure</th>
               <th>Cost</th>
               <th>Qty</th>
               <th>Total</th>
             </tr>
           </thead>
           <tbody>
-            {treatmentIDs.map((t, i) => (
-              <PayRow tid={t} returnToParent={updatePriceOfTreatment} key={i} />
+            {treatments.map((data, index) => (
+              <PayRow data={data} index={index} returnToParent={updatePriceOfTreatment} />
             ))}
           </tbody>
         </table>
-        <p>Sub total: {total.sub_total}</p>
-        <p>Discount: {total.discount}</p>
-        <p>Grand total: {total.grand_total}</p>
+
+        <div style={{ position: 'absolute', right: '0' }}>
+          <p>Sub Total: {total.sub_total}</p>
+          <label>Discount (%)</label>&nbsp;&nbsp;&nbsp;&nbsp;
+          <input
+            type="number"
+            min={0}
+            className="input-bar small-input-bar"
+            style={{ width: '4vw' }}
+            name="discount"
+            value={total.discount}
+            onInput={handleInput}
+          />
+          <p>Grand Total: {total.grand_total}</p>
+        </div>
+
+        <div>
+          <h4>Payment Details</h4>
+          <label>Payment method</label>&nbsp;&nbsp;
+          <select
+            className="input-select"
+            style={{ width: '8vw' }}
+            name="payment_method"
+            onInput={handleInput}
+          >
+            {config.PAYMENT_METHODS.map((method) => (
+              <option value={method}>{method}</option>
+            ))}
+          </select>
+          <br />
+          <br />
+          <label>Payment ID (optional)</label>&nbsp;&nbsp;
+          <input
+            type="text"
+            className="input-bar"
+            name="payment_id"
+            value={payment.payment_id}
+            onInput={handleInput}
+          />
+        </div>
+
+        <br />
+        <div>
+          <button onClick={handleSubmit} className="primary">
+            SUBMIT
+          </button>
+        </div>
       </div>
+      <Modal
+        show={modal.show}
+        message={modal.message}
+        clickHandler={() => setModal({ ...modal, show: false })}
+      />
     </Fragment>
   );
 }
