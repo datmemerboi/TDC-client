be7234434b6c8bf94808d1162da5fe0465e81afe
datmemerboi <shravanssd@gmail.com>
2022-02-12 18:59:16 +0530

    feat: Calendar index page

--- pages/calendar/index.js
+++
@@ -1,6 +0,0 @@
-// import Calendar from '../../components/calendar/calendar';
-import Calendar from '../../components/calendar';
-
-export default function CalendarIndex() {
-  return <Calendar />;
-}
